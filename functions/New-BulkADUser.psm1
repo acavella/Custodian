ce5224d6e907480d86eb8727e04c908d2dfe944e
Tony Cavella <9434191+acavella@users.noreply.github.com>
2019-11-08 07:13:02 -0500

    Added Import-CSV

--- functions/New-BulkADUser.psm1
+++ functions/New-BulkADUser.psm1
@@ -14,7 +14,18 @@ Function New-BulkADUser
     	[parameter(Mandatory=$true)]
         [String] $Path
 	)
-    Begin{}
-    Process{}
+    Begin{
+		Import-Module ActiveDirectory
+		$User=@()
+		$Pass=@()
+		Import-Csv -Path $Path -Delimiter "," |`
+    	ForEach-Object {
+        	$User += $_.Username
+        	$Pass += $_.Password
+    	}
+	}
+    Process{
+
+	}
     End{}
 }
